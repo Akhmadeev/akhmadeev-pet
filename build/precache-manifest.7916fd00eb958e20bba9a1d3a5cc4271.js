80a5ca76a099816ae62b4f24298192b85bbcf153
CodeSandbox <support@codesandbox.io>
2023-04-03 17:27:20 +0000

    Initial commit

---
+++ build/precache-manifest.7916fd00eb958e20bba9a1d3a5cc4271.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccfa8b17b980baa20f69",
+    "url": "/static/css/main.402f279d.chunk.css"
+  },
+  {
+    "revision": "ccfa8b17b980baa20f69",
+    "url": "/static/js/main.8a5487a2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b38917830af5594352eb",
+    "url": "/static/js/2.7aea2609.chunk.js"
+  },
+  {
+    "revision": "6979a3a3cb664c989010b339ecb86111",
+    "url": "/static/media/LinkedInIcon.6979a3a3.svg"
+  },
+  {
+    "revision": "aa73e13a0196e87c2e34d5372ef20334",
+    "url": "/static/media/TelegramIcon.aa73e13a.svg"
+  },
+  {
+    "revision": "8569a9fdfafa92abb62144955a352c9f",
+    "url": "/static/media/EmailIcon.8569a9fd.svg"
+  },
+  {
+    "revision": "53f757e43866be5314f18d6c12477c48",
+    "url": "/index.html"
+  }
+];
